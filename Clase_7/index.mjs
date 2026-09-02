578c2586059c5f691f384776a498049b4532d2a4
mateobonavia <mateobonavia@gmail.com>
2026-09-01 22:10:54 -0300

    mas http

--- Clase_7/index.mjs
+++ Clase_7/index.mjs
@@ -1,8 +1,18 @@
 import http, { createServer } from "node:http";
 
+console.log("Inicio");
 const servidor = createServer((req, res) => {
-    console.log(req);
-    res.end("Hola hola");
+    if (req.url === '/') {
+        console.log(`URL => ${req.url} - PETICION => ${req.method}`);
+        return res.end("Hola hola");
+    } 
+
+    if (req.url === '/profile') {
+        console.log(`URL => ${req.url} - PETICION => ${req.method}`);
+        return res.end(`Perfil`);
+    }
+
+    res.end(`Error => ${res.statusCode}`);
 });
 
 const port = 3000;
