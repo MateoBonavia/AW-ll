1395d0ed548f922c9277d098984237b2b5976c0d
mateobonavia <mateobonavia@gmail.com>
2026-09-17 19:23:51 -0300

    POST para ingresar mas data

--- Clase_11/index.mjs
+++ Clase_11/index.mjs
@@ -4,6 +4,13 @@ const app = express();
 
 const port = 3000;
 
+const data = [
+  {
+    compu: "PC-72",
+    activo: true,
+  },
+];
+
 app.listen(3000, () => console.log(`Servidor escuchando en el puerto ${port}`));
 
 app.use(express.json());
@@ -11,8 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/estado", (req, res) => {
-  res.json({
-    compu: "PC-72",
-    activo: true,
-  });
+  res.json(data);
+});
+
+app.post("/estado", (req, res) => {
+  data.push(req.body);
+
+  res.json(data);
 });
