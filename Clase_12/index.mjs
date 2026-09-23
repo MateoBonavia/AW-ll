3461adbd8dbbabd349b66a7a2737b9e7e7f7fe01
mateobonavia <mateobonavia@gmail.com>
2026-09-22 22:08:15 -0300

    Implementado middleware

--- Clase_12/index.mjs
+++ Clase_12/index.mjs
@@ -11,18 +11,28 @@ const data = [
   },
 ];
 
-app.listen(3000, () => console.log(`Servidor escuchando en el puerto ${port}`));
-
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
+
+const verifyKey = (req, res, next) => {
+  const key = req.body["key"];
+  if (key === "RacingCampeonCopaArgentina2026") {
+    next();
+  } else {
+    res.status(403).json({ error: "Key invalida" });
+  }
+};
 
 app.get("/estado", (req, res) => {
   res.json(data);
 });
 
-app.post("/estado", (req, res) => {
-  data.push(req.body);
+app.post("/estado", verifyKey, (req, res) => {
+  const { key, ...newData } = req.body;
+  data.push(newData);
 
   res.json(data);
 });
+
+app.listen(3000, () => console.log(`Servidor escuchando en el puerto ${port}`));
